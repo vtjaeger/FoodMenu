ff5117adff0d9ddec90310f6ddb682189b0c43f8
vtjaeger <viniciustjaeger@gmail.com>
2024-02-02 09:13:19 -0300

    starting project

---
+++ src/main/java/com/cardapio/controlllers/FoodController.java
@@ -0,0 +1,11 @@
+package com.cardapio.controlllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
+public class FoodController {
+
+
+}
